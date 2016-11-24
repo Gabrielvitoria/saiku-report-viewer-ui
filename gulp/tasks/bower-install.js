01b45bcb2ab1ed25096ff6795e383f1f38edbf83
brenopolanski <breno.polanski@gmail.com>
2016-11-23 22:07:47 -0300

    SREP-3 #comment Add tasks with gulp and assets

---
+++ gulp/tasks/bower-install.js
@@ -0,0 +1,73 @@
+/**
+ *   Copyright 2016 OSBI Ltd
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+'use strict';
+
+// Necessary Plugins
+var gulp    = require('gulp');
+var plumber = require('gulp-plumber');
+var bower   = require('gulp-bower');
+var paths   = require('../paths');
+
+// Call Bower
+gulp.task('bower', function() {
+  return bower()
+    .pipe(gulp.dest(paths.source.bowerDir));
+});
+
+// Call Font Awesome
+gulp.task('fontAwesome', ['bower'], function() {
+  gulp.src(paths.source.bowerDir + '/font-awesome/css/font-awesome.min.css')
+    .pipe(plumber())
+    .pipe(gulp.dest(paths.dest.css));
+  gulp.src(paths.source.bowerDir + '/font-awesome/fonts/**.*')
+    .pipe(plumber())
+    .pipe(gulp.dest(paths.dest.fonts));
+});
+
+// Call jQuery
+gulp.task('jquery', ['bower'], function() {
+  gulp.src(paths.source.bowerDir + '/jquery/dist/jquery.min.js')
+    .pipe(plumber())
+    .pipe(gulp.dest(paths.dest.js));
+});
+
+// Call Bootstrap
+gulp.task('bootstrap', ['bower'], function() {
+  gulp.src(paths.source.bowerDir + '/bootstrap/dist/css/bootstrap.min.css')
+    .pipe(plumber())
+    .pipe(gulp.dest(paths.dest.css));
+  gulp.src(paths.source.bowerDir + '/bootstrap/dist/fonts/**.*')
+    .pipe(plumber())
+    .pipe(gulp.dest(paths.dest.fonts));
+  gulp.src(paths.source.bowerDir + '/bootstrap/dist/js/bootstrap.min.js')
+    .pipe(plumber())
+    .pipe(gulp.dest(paths.dest.js));
+});
+
+// Call PDFJS
+gulp.task('pdfjs', ['bower'], function() {
+  gulp.src(paths.source.bowerDir + '/pdfjs-dist/build/pdf.js')
+    .pipe(plumber())
+    .pipe(gulp.dest(paths.dest.js));
+  gulp.src(paths.source.bowerDir + '/pdfjs-dist/build/pdf.worker.js')
+    .pipe(plumber())
+    .pipe(gulp.dest(paths.dest.js));
+});
+
+// Call Bower Install
+module.exports = gulp.task('bower-install',
+  ['bower', 'fontAwesome', 'jquery', 'bootstrap', 'pdfjs']);
